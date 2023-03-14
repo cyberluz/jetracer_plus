94906efb1792c210cd594d460524328ffb983832
Ford Davis <ford.davis@nexgenacademy.com>
2023-03-14 08:32:55 -0600

    Create  my first jetracer program.py
    created first test script

---
+++  my first jetracer program.py
@@ -0,0 +1,13 @@
+
+#Pathfinders_vehicle_control_FGD
+
+#importing_required_libraries
+from jetracer.nvidia_racecar import NvidiaRacecar
+
+#Instantiate the object for Object Oriented Programming (OOP)
+car = NvidiaRacecar()
+
+#Our first command for the vehicle
+car.steering=0.3
+
+#car.steering=0.0
