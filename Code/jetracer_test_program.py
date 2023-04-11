3e4201dab83e144cb27719f1e434ffb7e5e43366
Ford Davis <ford.davis@nexgenacademy.com>
2023-04-10 20:51:37 -0600

    reset to github
    reset to new jetracer gitub repo

---
+++ Code/jetracer_test_program.py
@@ -0,0 +1,14 @@
+
+#Pathfinders_vehicle_control_FGD
+
+#importing_required_libraries
+from jetracer.nvidia_racecar import NvidiaRacecar
+
+#Instantiate the object for Object Oriented Programming (OOP)
+car = NvidiaRacecar()
+
+#Our first command for the vehicle
+car.steering=0.8
+
+#car.steering=0.8
+#
