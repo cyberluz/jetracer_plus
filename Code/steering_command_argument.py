87ca8a5556521e4a91abe64a31648c93245e2da0
Ford Davis <ford.davis@nexgenacademy.com>
2023-04-10 21:25:10 -0600

    fixing and upgrading this CLA script
    add for class tomorrow

--- Code/steering_command_argument.py
+++ Code/steering_command_argument.py
@@ -4,8 +4,9 @@
 # Learning how to use python to control the JetRacer
 # This implementation includes an input argument parser
 
-# Importing required libraries 
-from jetracer.nvidia_racecar Import NvidiaRacecar
+# Importing required libraries
+# note "jetracer.nvidia_racecar" library is only on robot
+from jetracer.nvidia_racecar import NvidiaRacecar
 import argparse
 
 # Instantiate the racecar module as an object for Object Oriented Programming (OOP)
@@ -14,8 +15,7 @@ car = NvidiaRacecar()
 # Instantiate the parser
 parser = argparse.ArgumentParser()
 
-#Create the parser
-
+#Adding an argument to the parser
 parser.add_argument('--value', type=float, required=True)# Parser for servo command
 
 # Parse the command line arguments
@@ -26,4 +26,7 @@ car.steering = args.value
 
 # formerly: car.steering= 0.3 or whatever, but now a dynamic args.value contains the 
 # value fed from the data stream
-#print = ("The command servo value is: ", args.value )
+print = ("The command servo value is: ", args.value )
+
+#example of a command line argument (CLA) two parts:flag (1 or 2 dashes), value
+#  python3 steering_command_argument.py --value 0.3
